6a8793314f8e96fc4f95d873e88021551d85ebba
Brian Yu <brianyu8@gmail.com>
2015-12-15 15:58:54 -0500

    stufdf

--- othello/othello.py
+++ othello/othello.py
@@ -1,5 +1,7 @@
 import sys
 
+cellNeighbors = {i: {j for j in range(64) if j==i+8 or j==i-8 or j==i-1 or j==i+1 or j==i+9 or j==i+7 or j==i-9 or j==i-7 } for i in range(64)}
+
 def display(board):
     border = ["-" for i in range(24)]
     border = "   {}".format("".join(border))
@@ -14,8 +16,7 @@ def display(board):
     print(cols)
 
 
-
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
     if len(sys.argv[1]) == 64:
         board = sys.argv[1]
         valid = True
@@ -27,3 +28,54 @@ if len(sys.argv) > 1:
                 break
         if valid:
             display(board)
+
+def findPossible(board, pos, side):
+    global cellNeighbors
+    global possible
+    
+    '''
+        Only recurse to positions that are direcly opposite the original pos!!! this way there is a straight path and not just neighbors
+
+    '''
+
+    if pos not in range(64):
+        return ""
+    if board[pos] == ".":
+        return ""
+    for i in cellNeighbors[pos]:
+        fp = findPossible(board, i, side)
+        if fp != "":
+          return fp
+
+
+if len(sys.argv) == 3:
+    side = sys.argv[2]
+    board = sys.argv[1]
+    sidePos = [i for i in range(len(board)) if board[i]==side]
+    possible = []
+    for pos in sidePos:
+        possible.append(findPossible(board, pos, side))
+    display(board)
+    print(possible)
+
+    
+#print(cellNeighbors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#eof
