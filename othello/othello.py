d225f97c6cd57ee42742730c37b32bf53add1e6b
Brian Yu <brianyu8@gmail.com>
2015-12-16 15:09:38 -0500

    cellStraights

--- othello/othello.py
+++ othello/othello.py
@@ -1,6 +1,8 @@
 import sys
 
-cellNeighbors = {i: {j for j in range(64) if j==i+8 or j==i-8 or j==i-1 or j==i+1 or j==i+9 or j==i+7 or j==i-9 or j==i-7 } for i in range(64)}
+cellNeighbors = {i: {j for j in range(64) if j!=i and (j==i+8 or j==i-8 or j==i-1 or j==i+1 or j==i+9 or j==i+7 or j==i-9 or j==i-7) } for i in range(64)}
+cellStraights = {i: {j for j in range(64) if j!=i and (j%8==i%8 or int(j/8) == int(i/8))} for i in range(64)}
+opposite = {"X": "O", "O":"X"}
 
 def display(board):
     border = ["-" for i in range(24)]
@@ -18,49 +20,51 @@ def display(board):
 
 if len(sys.argv) == 2:
     if len(sys.argv[1]) == 64:
-        board = sys.argv[1]
+        board = sys.argv[1].upper()
         valid = True
         for char in board:
-            up = char.upper()
-            if up != 'X' and up != 'O' and up != '.':
+            if char != 'X' and char != 'O' and char != '.':
                 valid = False
                 print("Invalid Board.")
                 break
         if valid:
             display(board)
 
-def findPossible(board, pos, side):
+def findPossible(board, pos, origin, side):
     global cellNeighbors
     global possible
+
+    opposite = opposite[side]
+           
+    if pos is None:
+        if opposite not in board:
+            return ""
+        for pos in cellNeighbors[origin]:
+            if board[pos] == opposite:
+                return findPossible(board, pos, origin, side)
     
-    '''
-        Only recurse to positions that are direcly opposite the original pos!!! this way there is a straight path and not just neighbors
-
-    '''
-
-    if pos not in range(64):
-        return ""
     if board[pos] == ".":
+        possible.append(pos)
         return ""
-    for i in cellNeighbors[pos]:
-        fp = findPossible(board, i, side)
-        if fp != "":
-          return fp
+
+    else:
+        for i in cellNeighbors[pos]:
+            pass 
 
 
 if len(sys.argv) == 3:
-    side = sys.argv[2]
-    board = sys.argv[1]
+    side = sys.argv[2].upper()
+    board = sys.argv[1].upper()
     sidePos = [i for i in range(len(board)) if board[i]==side]
     possible = []
-    for pos in sidePos:
-        possible.append(findPossible(board, pos, side))
     display(board)
+    for pos in sidePos:
+        findPossible(board, None, pos, side)
     print(possible)
 
     
 #print(cellNeighbors)
-
+print(cellStraights)
 
 
 
