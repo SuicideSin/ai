13dab9270842921805e964820a5850273f6dea6c
Brian Yu <brian@byu.io>
2015-12-17 11:57:45 -0500

    Got possible moves working othello

--- othello/othello.py
+++ othello/othello.py
@@ -2,7 +2,46 @@ import sys
 
 cellNeighbors = {i: {j for j in range(64) if j!=i and (abs(int(j/8)-int(i/8)) <= 1 and abs((j%8)-(i%8)) <= 1) } for i in range(64)}
 
-cellStraights = {i: {j for j in range(64) if j!=i and (int(j/8)==int(i/8) or (j%8)==(i%8) or ((abs(int(j/8)-int(i/8)) == abs((j%8)-(i%8)))))} for i in range(64)}
+# cellStraights = {i: {j for j in range(64) if j!=i and (int(j/8)==int(i/8) or (j%8)==(i%8) or ((abs(int(j/8)-int(i/8)) == abs((j%8)-(i%8)))))} for i in range(64)}
+
+cellPaths = {}
+#Perhaps make cellPaths a dictionary of 8 sets. each set is one direction from the origin. all new positions in the recursive function must go into the correct set.
+
+for i in range(64):
+    paths = [[] for j in range(8)]
+    for j in range(64):
+        jrow = int(j/8)
+        jcol = j%8
+        irow = int(i/8)
+        icol = i%8
+        #same row to the left
+        if jrow == irow and jcol < icol:
+            paths[0].append(j)
+        #same row to the right
+        elif jrow == irow and jcol > icol:
+            paths[1].append(j)
+        #same col upward
+        elif jcol == icol and jrow > irow:
+            paths[2].append(j)
+        #same col downward
+        elif jcol == icol and jrow < irow:
+            paths[3].append(j)
+        #diagonal
+        elif abs(jrow - irow) == abs(jcol - icol):
+            #NW
+            if jrow < irow and jcol < icol:
+                paths[4].append(j)
+            #NE
+            if jrow < irow and jcol > icol:
+                paths[5].append(j)
+            #SW
+            if jrow > irow and jcol < icol:
+                paths[6].append(j)
+            #SE
+            if jrow > irow and jcol > icol:
+                paths[7].append(j)
+
+    cellPaths[i] = paths
 
 oppositeSide = {"X": "O", "O":"X"}
 
@@ -39,8 +78,8 @@ if len(sys.argv) == 2:
         if valid:
             display(board)
 
-def findPossible(board, pos, origin, path, possible):
-    global cellNeighbors, cellStraights, oppositeSide
+def findPossible(board, pos, origin, visited, path, possible):
+    global cellNeighbors, cellPaths, oppositeSide #, cellStraights
 
     opposite = oppositeSide[board[origin]]
 
@@ -49,18 +88,21 @@ def findPossible(board, pos, origin, path, possible):
             return
         for pos in cellNeighbors[origin]:
             if board[pos] == opposite:
-                path[pos] = origin
-                findPossible(board, pos, origin, path, possible)
+                visited[pos] = origin
+                path = []
+                for route in cellPaths[origin]:
+                    if pos in route:
+                        path = route
+                findPossible(board, pos, origin, visited, path, possible)
 
-    elif board[pos] == "." and board[path[pos]] == opposite:
+    elif board[pos] == "." and board[visited[pos]] == opposite:
         possible.add(pos)
         return
-    '''Needs to commit to a direction!!! Cant't be in any direction of the origin, must go in the same direction as the previous one'''
     else:
         for i in cellNeighbors[pos]:
-            if i not in path and i in cellStraights[origin]:
-                path[i] = pos
-                findPossible(board, i, origin, path, possible)
+            if i not in visited and i in path:
+                visited[i] = pos
+                findPossible(board, i, origin, visited, path, possible)
 
 if len(sys.argv) == 3:
     side = sys.argv[2].upper()
@@ -70,10 +112,11 @@ if len(sys.argv) == 3:
     total = set()
     for pos in sidePos:
         possible = set()
-        findPossible(board, None, pos, {}, possible)
+        findPossible(board, None, pos, {}, {}, possible)
         total = total | possible
-        print(coord(pos))
+        print("Origin: {}".format(coord(pos)))
+        print("Endpoints:")
         for pos in sorted(possible):
             print('\t{}'.format(coord(pos)))
-
+        print()
     display(board, total, side)
--- othello/positions.py
+++ othello/positions.py
@@ -4,6 +4,46 @@ cellNeighbors = {i: {j for j in range(64) if j!=i and (abs(int(j/8)-int(i/8)) <=
 
 cellStraights = {i: {j for j in range(64) if j!=i and (int(j/8)==int(i/8) or (j%8)==(i%8) or ((abs(int(j/8)-int(i/8)) == abs((j%8)-(i%8)))))} for i in range(64)}
 
+cellPaths = {}
+#Perhaps make cellPaths a dictionary of 8 sets. each set is one direction from the origin. all new positions in the recursive function must go into the correct set.
+
+for i in range(64):
+    paths = [[] for j in range(8)]
+    for j in range(64):
+        jrow = int(j/8)
+        jcol = j%8
+        irow = int(i/8)
+        icol = i%8
+        #same row to the left
+        if jrow == irow and jcol < icol:
+            paths[0].append(j)
+        #same row to the right
+        elif jrow == irow and jcol > icol:
+            paths[1].append(j)
+        #same col upward
+        elif jcol == icol and jrow > irow:
+            paths[2].append(j)
+        #same col downward
+        elif jcol == icol and jrow < irow:
+            paths[3].append(j)
+        #diagonal
+        elif abs(jrow - irow) == abs(jcol - icol):
+            #NW
+            if jrow < irow and jcol < icol:
+                paths[4].append(j)
+            #NE
+            if jrow < irow and jcol > icol:
+                paths[5].append(j)
+            #SW
+            if jrow > irow and jcol < icol:
+                paths[6].append(j)
+            #SE
+            if jrow > irow and jcol > icol:
+                paths[7].append(j)
+
+    cellPaths[i] = paths
+
+
 opposite = {"X": "O", "O":"X"}
 
 def coord(position):
@@ -71,3 +111,10 @@ if len(sys.argv) > 1:
             pos = int(index)
             print("Row: {}".format(int(pos/8)))
             print("Col: {}".format(pos%8))
+    elif sys.argv[1] == "p":
+        print(cellPaths)
+        display([str(i) for i in range(64)])
+        while True:
+            index = input("Which index? ")
+            pos = int(index)
+            print(cellPaths[pos])
